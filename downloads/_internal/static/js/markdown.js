5e49130d78e84b17e4d945d1932dc87e7b54a04a
hamelumster <kinetic487872@gmail.com>
2025-10-06 22:50:03 +0300

    add Meeseeks Box.zip

--- downloads/_internal/static/js/markdown.js
+++
@@ -1,7 +0,0 @@
-// настройки marked
-marked.setOptions({ gfm: true, breaks: true, smartypants: true });
-
-// helper: markdown -> безопасный HTML
-window.md = function (html) {
-  return DOMPurify.sanitize(marked.parse(html || ''));
-};
